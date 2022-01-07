a27e5ebd42f9d0abbfb6fa9333d3ab3f24ac925d
unknown <you@example.com>
2022-01-07 14:01:58 +0600

    flage

--- src/Pages/Flag/Flag.js
+++ src/Pages/Flag/Flag.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import flag from "../../img/flag.png";
+import flag from "../../img/flg.png";
 import "./Flag.css";
 const Flag = () => {
   return (
     <div>
       <Container>
-        <div className="bg-flag"></div>
+        <div className="my-5">
+          <img width="100%" src={flag} alt="" />
+        </div>
       </Container>
     </div>
   );
